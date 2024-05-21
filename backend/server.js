c2da6b5a4899bc796a3753bd4341d06457c2bd42
Harsh Singh <sengarharshsinghrajput@gmail.com>
2024-05-21 18:49:24 +0530

    chat end-point created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express')
-
+const dotenv=require('dotenv')
+const chats =require("./data/data.js")
 const app = express()
 
-app.listen(5000,
-    console.log("Server started on port 5000")
-)
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 app.get("/", (req,res) => {
     res.send("hey! got tested successfully")
 })
+
+app.get("/api/chat", (req,res) => {
+    res.send(chats)
+    console.log("test")
+})
+app.listen(PORT,
+    console.log(`Server started on port ${PORT}`)
+)
