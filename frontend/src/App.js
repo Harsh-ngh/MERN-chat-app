2cc3ba840364dc7995c6bb933b5c12d90d899a82
Harsh Singh <sengarharshsinghrajput@gmail.com>
2024-05-21 19:40:27 +0530

    HomePAge and ChatPage created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+import Home from './Pages/Home';
+import Chat from './Pages/Chat';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    hello
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/chats" component={Chat} />
+      </Switch>
     </div>
   );
 }
